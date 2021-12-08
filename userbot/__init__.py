3fdf4316c3d1385fbc3353598a46a3dac246b4e6
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 19:21:02 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -195,7 +195,7 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 VEGETA_TEKS_KOSTUM = os.environ.get("SAYONARA_TEKS_KOSTUM") or "ㅤ"
 
 # Untuk Melihat Repo
-REPO_NAME = os.environ.get("REPO_NAME) or "💀BAGAS-USERBOT💀
+REPO_NAME = os.environ.get("REPO_NAME") or "💀sᴀʏᴏɴᴀʀᴀ-ᴜsᴇʀʙᴏᴛ💀"
 
 # Default .alive Name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
