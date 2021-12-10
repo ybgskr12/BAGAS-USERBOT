870bfe481b410d44d252f79f9d1496a834b07446
yusupbagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2021-12-10 09:22:10 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -28,15 +28,15 @@ from userbot.events import register
 
 absen = [
     "**Hadir Ganteng** 🥵",
-    "**Hadir Bang Sayo** 😎",
+    "**Hadir Bang Bagas** 😎",
     "**Hadir Kak** 😉",
-    "**Hadir Bang Sayo** 😁",
+    "**Hadir Sayang** 😘",
     "**Hadir Kak Maap Telat** 🥺",
 ]
 
 pacar = [
-    "**Kamu mau jadi pacar aku ga?** 💘",
-    "**Memek mending sama aku** 😎",
+    "**Bagas mau jadi pacar aku ga?** 💘",
+    "**Bagas mending sama aku** 😎",
     "**Hai ganteng** 🐷",
     "**Mau ga bang jadi pacar aku?** 😁",
     "**Mending pc aku bang** 🥺",
@@ -199,7 +199,7 @@ async def redis(pong):
     await asyncio.sleep(1)
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**sᴀʏᴏɴᴀʀᴀ-ᴜsᴇʀʙᴏᴛ.... !!**\n**SAYONARA ONLINE** : `%sms`\n**UPTIMES PREEM** : `{uptime}🔥`" % (duration))
+    await pong.edit(f"**Bagas-Userbot.... !!**\n**BAGASKARA ONLINE** : `%sms`\n**UPTIMES PREEM** : `{uptime}🔥`" % (duration))
 
 @register(outgoing=True, pattern="^.crot$")
 async def redis(pong):
@@ -242,7 +242,7 @@ async def redis(pong):
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**{ALIVE_NAME}**        \n"
                     f"**➾Kecepatan : ** '%sms'  \n"
-                    f"**➾Branch : ** 'Sayonara-Userbot` \n" % (duration))
+                    f"**➾Branch : ** 'Bagas-Userbot` \n" % (duration))
 
 @register(outgoing=True, pattern="^.xping$")
 async def redis(pong):
@@ -260,7 +260,7 @@ async def redis(pong):
     await pong.edit("__Loading...__")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**💀sᴀʏᴏɴᴀʀᴀ-ᴜsᴇʀʙᴏᴛ💀**\n"
+    await pong.edit(f"**💀Bagas-Userbot💀**\n"
                     f"➾ __Signal__    __:__ "
                     f"`%sms` \n"
                     f"➾ __Uptime__ __:__ "
@@ -281,7 +281,7 @@ async def redis(pong):
     await asyncio.sleep(2)
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**🔥sᴀʏᴏɴᴀʀᴀ-ᴜsᴇʀʙᴏᴛ🔥**\n\n"
+    await pong.edit(f"**🔥Bagas-Userbot🔥**\n\n"
                     f"** ▹  Sɪɢɴᴀʟ   :** "
                     f"`%sms` \n"
                     f"** ▹  Uᴘᴛɪᴍᴇ  :** "
@@ -299,7 +299,7 @@ async def redis(pong):
     await pong.edit("**◕‿- PONG!!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**🔥sᴀʏᴏɴᴀʀᴀ-ᴜsᴇʀʙᴏᴛ🔥**\n"
+    await pong.edit(f"**🔥Bagas-Userbot🔥**\n"
                     f"❊ **Ping:** "
                     f"`%sms` \n"
                     f"❊ **Uptime:** "
@@ -374,7 +374,7 @@ async def redis(pong):
     await pong.edit(
         f"**{ALIVE_NAME}**        \n"
         f"**➾Kecepatan : ** %sms  \n"
-        f"**➾Branch : ** Sayonara-Userbot \n" % (duration)) 
+        f"**➾Branch : ** Bagas-Userbot \n" % (duration)) 
 
 @register(outgoing=True, pattern="^!uping$")
 async def pingme(pong):
