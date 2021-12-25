dfbb70a7e51929540d12e168928fcf8fdd1c65d3
Bagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2021-12-25 16:28:37 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -6,11 +6,11 @@ from userbot.events import register
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Hai Perkenalkan Namaku Sayonara`")
+    await typew.edit("`Hai Perkenalkan Namaku Bagaskara`")
     sleep(3)
-    await typew.edit("20 Tahun`")
+    await typew.edit("22 Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Di Riau, Salam Kenal:)`")
+    await typew.edit("`Tinggal Di Jawa Tengah, Solo, Salam Kenal:)`")
 # Create by myself @localheart
 
 
