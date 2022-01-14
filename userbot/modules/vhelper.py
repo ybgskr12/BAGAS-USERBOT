29db19e82c2ad8431a53de6784a6ee407f2fed5e
Bagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2022-01-15 00:21:39 +0700

    Update vhelper.py

--- userbot/modules/vhelper.py
+++ userbot/modules/vhelper.py
@@ -12,22 +12,22 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.rhelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
-        "\n[XSVSHACKER😈](t.me/gsahmanja)"
-        "\n\n[SUPPORT](https://t.me/StaryWild)"
-        "\n\n[CHANNEL](https://t.me/codersUpdates)")
+        "\n[BAGASKARA😈](t.me/ybgskr_ex)"
+        "\n\n[SUPPORT](https://t.me/allfucek)"
+        "\n\n[CHANNEL](https://t.me/loveisfuckedup)")
 
 
 @register(outgoing=True, pattern="^.rvars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Randi356/VEGETA-USERBOT/VEGETA-USERBOT/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ybgskr12/BAGAS-USERBOT/BAGAS-USERBOT/varshelper.txt)")
 
 
 CMD_HELP.update({
     "vegetahelper":
     "`.helpmy`\
-\nPenjelasan: Bantuan Untuk VEGETA-USERBOT.\
+\nPenjelasan: Bantuan Untuk BAGAS-USERBOT.\
 \n`.rvars`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
