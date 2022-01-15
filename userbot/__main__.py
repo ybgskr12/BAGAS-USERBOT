42b6c3113628c4165cc41b5a34deba2f94ab36d1
Bagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2022-01-16 06:46:17 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔘BAGAS-USERBOT🔘 🔥 V7.1 [TELAH AKTIF !!!!]")
+    f"🐧BAGAS-USERBOT🐧 🔥 V7.1 [TELAH AKTIF !!!!]")
 
 
 if len(argv) not in (1, 3, 4):
