56dc7512b6f63b71363a4a90ce71e978e449db8d
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-02-09 09:30:21 +0700

    ngetest

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -18,13 +18,12 @@
 
 import logging
 
-
 from userbot import BOT_USERNAME
 from userbot.events import register
 
 logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING)
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
 
 
 @register(outgoing=True, pattern=r"^\.helpme")
@@ -32,16 +31,16 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@Ram_ubot")
+            results = await event.client.inline_query(tgbotusername, "@levintapibot")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
         else:
             await event.edit(
-                "`Bot tidak berfungsi! Harap setel Token Bot dan Nama Pengguna dengan benar. Modul telah dihentikan.`"
+                "`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username dengan benar. Modul telah dihentikan.`"
             )
     except Exception:
         return await event.edit(
-            "`Anda tidak dapat Menggunakan Perintah .helpme, Bisa Jadi Grup Ini tidak Mensupport nya (Mungkin Anda Belum Menghidupakan Inline Mode Dan Inline Location Data)`"
+            "`Anda tidak dapat mengirim hasil sebaris dalam hal ini ke chat (disebabkan oleh Mengirim Inline Sebaris)`"
         )
