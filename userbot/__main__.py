a6c85ea25d123467fddb2924fa8317b3f0ea656b
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-10 22:38:41 +0700

    ```NEW UPDATED```

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -2,35 +2,43 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
+# by rendy
 """ Userbot start point """
 
+import sys
 from importlib import import_module
-from sys import argv
 
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
+from telethon.tl.functions.channels import InviteToChannelRequest
+from userbot import ALIVE_NAME, BOT_USERNAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES
-
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n Tip: Use Country Code along with number.' \
-             '\n or check your phone number and try again !'
-
+from userbot.utils.tools import hadeh_ajg
 try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
     bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
-
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
+    LOGS.info(f"⚡Bagas-Userbot⚡ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+except BaseException as e:
+    LOGS.info(str(e), exc_info=True)
+    sys.exit(1)
 
 
-LOGS.info(
-    f"⚡BAGASKARA-USERBOT⚡ V7.1 [SUDAH AKTIF!!]")
+async def bagas_ubot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"⚡ Bagas-Userbot Berhasil Diaktfikan ⚡\n╼┅━━━━━╍━━━━━┅╾\n❍▹ Bot Of : {ALIVE_NAME}\n❍▹ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n╼┅━━━━━╍━━━━━┅╾",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+    try:
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
+    except BaseException:
+        pass
 
-
-if len(argv) not in (1, 3, 4):
+bot.loop.run_until_complete(hadeh_ajg())
+bot.loop.run_until_complete(bagas_ubot_on())
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
