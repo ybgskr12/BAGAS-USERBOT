5a82c19b08b68b4538c32ffecbbc2e6c9e67c8f7
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-03-11 22:15:25 +0700

    add bacotan roas

---
+++ userbot/modules/roas.py
@@ -0,0 +1,108 @@
+# Bagaskara
+# Gausah kesini ngentot!!
+# NGEDIT CMD YG BENER KONTOL!!!
+# YANG HAPUS KREDIT GUA TANDAIN REPO LO
+
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+@register(outgoing=True, pattern='^.roas1(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**EH MANUSIA HARAM YANG KERJAANYA REBAHAN, GAUSAH SOK SOK MENILAI HIDUP ORANG DEH LO BANGSAT, LO SENDIRI AJA GA MAMPU BUAT MAKAN GOBLOK KARNA LU GEMBEL KERJA LO PASTI JADI MANUSIA SILVER YANG KAN, NAH MAKANYA DARIPADA KEBANYAKAN NGURUSIN HIDUP ORAMG, MENDING LU CAT BADAN LU BURUAN ABISTU KERJA DAH NGEMIS NGEMIS KALO GAK MATUNG DI PINGGIR JALAN, YAHAHA KAYA BOCAH GA PUNYA KREATIFITAS ATAU MEMANG LO PUNYA KELAINAN DISABILITAS, YANG KELIATAN IDIOT TUU, PASTI JUGA LU LULUSAN SLB YAKAN BAGIAN ORANG ORANG YG KENA GANGGUAN JIWA ATAU PUNYA KELAINAN TU YKANN YAHAHAHAH WAHYOUEEEE🤪🤪**")
+
+
+@register(outgoing=True, pattern='^.roas2(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**INI NI KALO ANAK LONTE BERHASIL DI LAHIRKAN ANJING, EMAK LO GABISA GUGURIN DAN LO LAHIR SEBAGAI ANAK HARAM, KASIAN BANGET LO ANJING DARI MASA KANDUNGAN LO DI EMAK LO, UDAH GADA BAPAK, YA IYALAH ANJING ORANG ANAK LONTE, TUBUH LO ITU ADALAH CAMPURAN DARI SEMUA PELANGGAN EMAK LO NGENTOD, NAH KEBETULAN PAS KEBABLASAN EMAK LO LUPA MINUM PIL KB, JADI HAMIL KAN, UDAH LA NGENTOD MENDING LO BUNUH DIRI AJA, GA PANTES BANGET LO DI DUNIA INI ANJING!!!**")
+
+
+@register(outgoing=True, pattern='^.roas3(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**WOI ALIANSI SAMPAH, NI DENGERIN YA, GC LO SEPI PASTI KAN, MAKANYA LO NGERUSUH DI GC ORANG TERUS NGAJAK WAR MANA PAKE CUAN LAGI, GUA TAU LU NGAJAK MAIN CUAN PASTI KARNA EKONOMI KELUARGALO LAGI RENDAH ANJING, YA IYALAH BAPAK LO PENGANGGURAN, EH GA DA BAPA KAYANYA, NAH EMAK LO LUMPUH KERJAAN NYA REBAHAN, MAU KEKAMAR MANDI AJA HARUS DI GOTONG KAN, YAHAHA CACAT BANGET ANJING, LO JUGA TERMASUK ORANG CACAT, MANA BIBIR SUMBING TANGAN TREMOR, UDAH COCOK BGT BUAT LO MATI BERDUA EMAK LO YAHAHAH NGENTOD ALIANSI SAMPAH!!!**")
+
+
+@register(outgoing=True, pattern='^.roas4(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MASIH JAMAN NGERUSUH SANA SINI, TERUS PAS DI LADENIN MALAH LARI?, KALO BRANI NGAJAK WAR ORANG YA PAKE KONSEKUENSI LAH TOLOL, KALO CUMA NGATA NGATAIN BEGITU, BOCIL FF JUGA BISA TOLOL, DAN KALO NGAJAK WAR TAPI ALIBI MULU, DI AJAKIN PAKE KONSEKUENSI KAGAK MAO, DI TANTANG MAIN SESI KABUR KABURAN MULU, MENDING LO WAR AJA SAMA BOCAH TK TOLOL, BRANI WAR LGSG AJA KE ALFAM BEGO, ITU JUGA KALO LU BRANI YA, KALO CUPU MAH BUBARIN AJA TU ALIANSI LO YG GA BERGUNA ITU ANIJINGGGGG!!!!**")
+
+
+@register(outgoing=True, pattern='^.roas5(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**NIH YA ANAK BABI, GUA KASIH TAU KE LU NIH, LU ITU DISINI CUMA JADI KACUNG GUA NGENTOD, YANG SABAN HARI GUA INJEK INJEK TU BATANG LEHER LU, YG TIAP HARI GUA SURUH SURUH NGELAPIN KAKI GUA, GAUSAH SOK KERAS TOLOL, BAPAK LO LAGI MULUNG NOH DI PEREMPATAN, NUNGGUIN SAMPAH SAMPAH PLASTIK TOLOL, MANA PAKE BAJU PARTAI UDH DEKIL BAT KUMEL ANJING KAYA MASA DEPAN LO YG SURAM, JANGAN BELAGU APA, GUA TAU LO SKOLA CUMA SAMPE LULUSAN SD KAN? YAHAHA KETAWAN BANGET DARI NASIB LU YG MENGENASKAN.**")
+
+
+@register(outgoing=True, pattern='^.roas6(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**YA AMPUN, UMUR UDAH TUA MAO MATI UDAH BAU TANAH ANJING, MASIH AJA NYENGGOL SANA SINI, PIKIRIN AJA KISAH HIDUP LU YG KELAM SAMPEL SEKARANG TOLOL, BANTUIN KEK KELUARGA LU YG KEKURANG EKONOMI, NGAPAIN KEK JUAL DIRI KEK, BIAR ADA HASIL BEGO, GUA SEBENARNYA KASIHAN SAMA LU SIH, UDAH JELEK ITEM MUKA UDAH KEK LINTINGAN DAKI BEGITU, BANGSAT BANGET NAJIS DEH GUA AMA LU.**")
+
+
+@register(outgoing=True, pattern='^.roas7(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**UDAH LAH, MENDING LO CABUT AJA DARI SINI, SOAL NYA GUA GAMAU ANJING LIAT MANUSIA SAMPAH KAYAK LO ADA DI TELEGRAM, LEBIH TEPAT NYA MENDING LO DOWNLOAD MICHAT NAH ABIS TU LU NGELOTE ANJING, COCOK BANGET SOAL NYA BUAT LO ASU!!!!**")
+
+
+@register(outgoing=True, pattern='^.roas8(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DARI PENILAIAN GUA NI YA, KEBANYAKAN DARI LO ITU CUMA JELEK NYA DOANG ANJING KAGA ADA BAGUS BAGUS NYA LO DI MATA GUA, KEK LIAT TAI BABI GUA KALO LIATIN MUKA LO, YA IYALAH ORANG TERLAHIR DARI EMAK YG PENYAKITAN, MEMEK NYA KENA RAJA SINGAN YAHAHA, TERLALU KASAR GUA, MAAF YA, EH TAPI EMG PANTES LO DI KATA KATAIN KAYA GITU, UDAH COCOK BANGET LO NYUSUL BAPAK LO SANA KE KUBURAN, MANA GA PERNAH DI DOAIN LAGI TU PASTI BAPAK LO YKAN, LAGI DI SIKSA MALAIKAT, GA ADA PENGAMPUNAN GARA GARA LO GA PERNAH DOA, YA GIMANA MAO DOA ORANG SELAMA LO HIDUP KERJAAN NYA CUMA MAEN TELEGRAM DOANG, NGEBACOT DOANG ANJINGG, GA PERNAH NGAJI GA PERNAH BELAJAR AGAMA PASTI, MAKANYA LO TUH MIS ATTITUDE, MIS AKHLAK PULA ADUHHHH ANAK NGENTOD.**")
+
+
+@register(outgoing=True, pattern='^.roas9(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GAUSAH SOK SOK AN NYENGGOL GUA ANJING, TAR GUA AJAKIN SESI, LO LARI, GUA AJAKIN BIKIN KONSEKUENSI LO BANYAK ALIBI, YAHAHA SSMPAH BANGET LO NGENTOD, UDAH LAH MENDING LO PERGI JAUH JAUH DARI MATA GUA, JIJI BAT LIATNYA CUIHHKKKK.**")
+
+
+@register(outgoing=True, pattern='^.roas10(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**NIH NIH GUA KASIH BANSOS DEH BUAT LO, LO BELOM PERNAH NGERASAIN MAKAN SARDEN KAN? YAHAHA KASIAN BANGET HIDUP LU YG SABAN HARI MAKAN NASI SAMA GAREM DOANG ANJING, MAKANYA OTAK LO BEGOOO, MANA PASTI SKALINYA MAKAN ENAK, LO CUMA GORENG TELOR AJA KAN, PAKE MICIN YG BANYAK, EMG BOCAH MICIN ANJING, RADA TOLOL SI GUA KALO NGELADENIN LO YG CUMA BISA NGANG NGENG NGONG DOANG ANJING, YA IYA KARNA LO, EMG GA ADA BAKAT APA APA ANJING DI TELEGRAM, CABUT BURUAN CABUT TAR GUA GBAN AMA GUA LO NGENTOD.**")
+
+
+@register(outgoing=True, pattern='^.roas11(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**EH BOCAH TOLOL  YANG MUKANYE RATA KEK TEMBOK PENJARA YANG BANYAK KERAK DAKI NARAPIDANA, MULUT LU BAU JIGONG, KUPING CONGEAN KEPALA PITAKAN PANTAT KORENGAN KAKI BUDUKAN KONTOL LO JUGA BISULAN, KALO UDEH BEJAT GAUSAH BEGAJULAN, LU KEBANYAKAN NONTON APLIKASI SI MONTOK SAMBIL COLI DIPOJOKAN DASAR YOHI YOHI YANG OTAK CETEK SKILL PENDEK MUKA GAPTEK YANG KALO MALEM KONTOLNYE DIPAKEIN SOPTEK, EH BOCAH YOHI DEKIL MAKENYE JANGAN KEBANYAKAN LIAT PENTIL, KALO UDEH TOLOL SADAR DIRI, LIAT MUKA LU UDEH KEK KETUMBAR, KETEK LU LDR, DASAR ANAK ANJING PAKIR MISKIN YANG BADANNYE BAU KRIKIL, DASAR ANJING BOCAH INGUSAN YANG KALO LIAT MEMEK LANGSUNG MIMISAN, DIKASIH PAP MUKA LANGSUNG SANGEAN DASAR ABIDIN ANJ,  GINI NIH KLO UDH MISKIN WAWASAN MISKIN PENDIDIKAN MISKIN 7 TURUNAN, EH KONTOL MAKANNYA KLO DISURUH SKLH TUH SKLH TOLOL JGN NGEMIS TRUS, MAK BAPA LU CAPE² NGEMIS KERJAAN DIRUMAH GUA CUMA BUAT PENDIDIKAN LU TOLOL, LU DISURUH SKLH MALAH NGEMIS, NGEGEMBEL, MALAH JADI MANUSIA SILVER LAGI TOLOL, INGET KLO UDH GA BERGUNA DIDUNIA TUH AOWKWWKWIK.**")
+
+@register(outgoing=True, pattern='^.roasndre(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**ini nih anak yang nyembah babi setiap hari yang kerjaan nya cuma makan kotoran gue, eh tolol malu napa udah juga nyembah babi gak punya emak malah sok sokan mau punya cewek, eh malu ama emak lu di neraka tolol, nih ye gue kasih tau ama lu anak hina yang lahir dari rahim anjing lu deger baik baik, orang tolol kek elu di dunia ini kaga di terima tolol wajah kontol yang beruntusan yang mana wajah nya aja kaga simetris kek pantat kuda, mending lu dirumah makan tai aja kerena apa bocah yang gak punya teman kek elu yang mana dakjal aja gak mau temanan ama lu malah di telegram mau sok kerasa tolol, ni ye gue kasih tau ama lu bocah hina nabi aja malu ada pegikut kek elu yang mana elu nya aja murtad malah jadi bocah tolol di telegram, dakjal aja gak mau temanan ama lu kerena lu itu hina tolol, malu kontol ett dah, ni ya orang miskin kek elu yang mana kerjaan nya cuma ngemis di kota Jakarta kota keras mana mampu lu idup di sana, kerena apa bocah kek elu itu di tabok sekali aja langsung mati tolol, ni lagi ye bocah kek elu yang hp aja hp mito malah sok keras kontol, suara lu aja masih kepedem ama suara gue sokakata masi ambrul aduk malah sok mau ngelawan gue, yang mana kodratnya gue itu tuhan lu tolol bocah kontol apa emak lu gua entotin sekalian yekan gua ijek ijek sekalian pepeknya yekan yehh pake sosoan ngtain org, de klo mau reply gua vn video muka jga ga ush vn yehh, najis bet najis gua goblok ngeladenin bocah bocah kek lu mah cwe jga hina bgt lu ngeladenin cwe kek lu mahapasi dek,dede dede ini masi apa pepeknya masi sempit kan jan sampe om entot yakan itu pepek lu itu yakan gua tau pepek lu itu masi sempit kalau gua masukin otong gua ntar berdarah ya ntar takutnya lu hamil yakan kalau ngga ini apa emak lu gua entotin sekalian yekan gua ijek ijek sekalian pepeknya yekan yehh pake sosoan ngtain org, de klo mau reply gua vn video muka jga ga ush vn yehh, najis bet najis gua goblok ngeladenin bocah bocah kek lu mah cwe jga hina bgt lu ngeladenin cwe kek lu mahapasi dek,dede dede ini masi apa pepeknya masi sempit kan jan sampe om entot yakan itu pepek lu itu yakan gua tau pepek lu itu masi sempit kalau gua masukin otong gua ntar berdarah ya ntar takutnya lu hamil yakan kalau ngga ini apa emak lu gua entotin sekalian yekan gua ijek ijek sekalian pepeknya yekan yehh pake sosoan ngtain org, de klo mau reply gua vn video muka jga ga ush vn yehh, najis bet najis gua goblok ngeladenin bocah bocah kek lu mah cwe jga hina bgt lu ngeladenin cwe kek lu mahapasi dek,dede dede ini masi apa pepeknya masi sempit kan jan sampe om entot yakan itu pepek lu itu yakan gua tau pepek lu itu masi sempit kalau gua masukin otong gua ntar berdarah ya ntar takutnya lu hamil yakan kalau ngga ini apa emak lu gua entotin sekalian yekan gua ijek ijek sekalian pepeknya yekan yehh pake sosoan ngtain org, de klo mau reply gua vn video muka jga ga ush vn yehh, najis bet najis gua goblok ngeladenin bocah bocah kek lu mah cwe jga hina bgt lu ngeladenin cwe kek lu mahapasi dek,dede dede ini masi apa pepeknya masi sempit kan jan sampe om entot yakan itu pepek lu itu yakan gua tau pepek lu itu masi sempit kalau gua masukin otong gua ntar berdarah ya ntar takutnya lu hamil yakan kalau ngga ini apa emak lu gua entotin sekalian yekan gua ijek ijek sekalian pepeknya yekan yehh pake sosoan ngtain org, de klo mau reply gua vn video muka jga ga ush vn yehh, najis bet najis gua goblok ngeladenin bocah bocah kek lu mah cwe jga hina bgt lu ngeladenin cwe kek lu mahapasi dek,dede dede ini masi apa pepeknya masi sempit kan jan sampe om entot yakan itu pepek lu itu yakan gua tau pepek lu itu masi sempit kalau gua masukin otong gua ntar berdarah ya ntar takutnya lu hamil yakan kalau ngga ini apa emak lu gua entotin sekalian yekan gua injek injek sekalian pepeknya yekan yehh pake sosoan ngtain org, de klo mau reply gua vn video muka jga ga ush vn yehh, najis bet najis gua goblok ngeladenin bocah bocah kek lu mah cwe jga hina bgt lu ngeladenin cwe kek lu mahapasi dek,dede dede ini masi apa pepeknya masi sempit kan jan sampe om entot yakan itu pepek lu itu yakan gua tau pepek lu itu masi sempit kalau gua masukin otong gua ntar berdarah ya ntar**")
+
+CMD_HELP.update({
+    "roas":
+    ".roas1\
+\nUsage:\
+\n\n.roas2\
+\nUsage:\
+\n\n.roas3\
+\nUsage:\
+\n\n.roas4\
+\nUsage:\
+\n\n.roas5\
+\nUsage:\
+\n\n.roas6\
+\nUsage:\
+\n\n.roas7\
+\nUsage:\
+\n\n.roas8\
+\nUsage:\
+\n\n.roas9\
+\nUsage:\
+\n\n.roas10\
+\nUsage:\
+\n\n.roas11\
+\nUsage:"
+})
