d58ff3b736f30d7b5ed2975613139fa492dcddec
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-03-20 00:16:21 +0700

    Create djancok.py

---
+++ userbot/modules/djancok.py
@@ -0,0 +1,42 @@
+#create by Bagaskara
+#Yang Copas Doang, Lu kontol
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.jancok(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`Woi Jancok !`")
+    sleep(2)
+    await typew.edit("`Lu Tau ga ? Lu itu Djancok Gausah Sok Keras !`")
+    sleep(1)
+    await typew.edit("`Muka Lu Kayak Kontol `")
+# Create by myself @ybgskr_ex 
+
+@register(outgoing=True, pattern='^.huek(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`Huek Cuih !`")
+    sleep(2)
+    await typew.edit("`Sempak Lu Kagak Pernah Ganti Ya !`")
+    sleep(2)
+    await typew.edit("`Pantes Kontol Lu Burik`")
+    sleep(2)
+    await typew.edit("`Order Vcs Aja Gaada Yang Mau Gblk !`")
+    sleep(2)
+    await typew.edit("`Makanya Jadi Orang Cakepan Dikit Kontol !`")
+    sleep(2)
+    await typew.edit("`Mending Lu Diem Unggas !!`")
+# Create by myself @ybgskr_ex
+
+
+CMD_HELP.jancok({
+    "djancok":
+    "`.jancok`\
+    \nUsage: liat aja.\
+    "`.huek`\
+    \nUsage: liat aja.\
